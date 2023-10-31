9b6eb9507847cb2c1becd9f3d63b105c0764f7ab
pogoparis <pogoparis@gmail.com>
2023-10-31 10:03:36 +0100

    bootstrap

--- src/main/java/fr/eni/airport/bo/Personne.java
+++ src/main/java/fr/eni/airport/bo/Personne.java
@@ -1,5 +1,6 @@
 package fr.eni.airport.bo;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -19,6 +20,7 @@ public class Personne {
     private String prenom;
     private Integer age;
 
+    @JsonIgnore
     @ManyToOne
     private Avion avion;
 
